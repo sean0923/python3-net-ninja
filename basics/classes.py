9f2b6b2aa0469e84094648f89ae10c11c747431b
Sean Hong <iamseanhong@gmail.com>
2018-06-07 21:18:42 -0700

    Learn class static instance methods $ class instance attributes

--- basics/classes.py
+++ basics/classes.py
@@ -11,6 +11,13 @@ class Person:
   def eat_food(self, food):
     print(f'{self.name} is eating {food}')
 
+  @classmethod
+  def print_class_method(cls):
+    print('print class method')
+
+  @staticmethod
+  def render_static_method():
+    print('I am staticmethod and I do not need arguments')
 
 person = Person('sean', 30)
 
@@ -20,3 +27,9 @@ person.eat_food('meat')
 
 # class level attribute
 print(Person.isHuman)
+
+Person.print_class_method()
+person.print_class_method()
+
+Person.render_static_method()
+person.render_static_method()
